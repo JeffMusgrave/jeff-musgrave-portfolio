b94cfd186227fc9bf20c97137f0c008dd56846b4
Jeff <jeff.musgrave@gmail.com>
2020-10-28 21:50:25 -0400

    re-ordered tabs on code page.

--- jm-react-site/src/data/codeContent.js
+++ jm-react-site/src/data/codeContent.js
@@ -1,8 +1,22 @@
 import { v4 as uuidv4 } from "uuid";
 
 export const content = {
+  "ML Pipeline": {
+    description: require("./markdown/code/01-MachineLearning.md"),
+    items: [
+      {
+        image: require("../img/portfolioItems/code/MLScripts.jpg"),
+        alt: "Machine Learning Video Pipeline",
+        thumbnail: require("../img/portfolioItems/code/thumbs/MLScripts_400.jpg"),
+        thumbnailAlt: "Machine Learning Video Pipeline",
+        init: require("../img/portfolioItems/code/init/MLScripts_init.jpg"),
+        id: uuidv4(),
+      },
+    ],
+    internalLinks: [{ title: "Video Reel", link: "/video", tab: 0 }],
+  },
   "JeffMusgrave.com": {
-    description: require("./markdown/code/01-ThisSite.md"),
+    description: require("./markdown/code/02-ThisSite.md"),
     items: [
       {
         title: "Jeff Musgrave - Website Code",
@@ -19,19 +33,5 @@ export const content = {
         url: "https://github.com/JeffMusgrave/jeff-musgrave-portfolio",
       },
     ],
-  },
-  "ML Pipeline": {
-    description: require("./markdown/code/02-MachineLearning.md"),
-    items: [
-      {
-        image: require("../img/portfolioItems/code/MLScripts.jpg"),
-        alt: "Machine Learning Video Pipeline",
-        thumbnail: require("../img/portfolioItems/code/thumbs/MLScripts_400.jpg"),
-        thumbnailAlt: "Machine Learning Video Pipeline",
-        init: require("../img/portfolioItems/code/init/MLScripts_init.jpg"),
-        id: uuidv4(),
-      },
-    ],
-    internalLinks: [{ title: "Video Reel", link: "/video", tab: 0 }],
   },
 };
